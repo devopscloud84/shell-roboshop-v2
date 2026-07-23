34be19f348b13aa71c240b55542c9881568280df
joindevopscloud <info@devoos.com>
2026-07-23 20:46:12 +0530

    shell

--- 05-catalogue.sh
+++ 05-catalogue.sh
@@ -1,3 +1,33 @@
+# #!/bin/bash
+
+# app_name=catalogue
+# source ./common.sh
+# check_root
+
+# app_setup
+# nodejs_setup
+# systemd_setup
+
+# cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+# VALIDATE $? "Added Mongo repo" 
+
+# dnf install mongodb-mongosh -y &>>$LOGS_FILE
+# VALIDATE $? "Installed MongoDB client"
+
+# INDEX=$(mongosh --host mongodb.daws8486.online --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+
+# if [ $INDEX -lt 0 ]; then
+#     mongosh --host mongodb.daws8486.online </app/db/master-data.js &>>$LOGS_FILE
+#     VALIDATE $? "Load Products"
+# else
+#     echo -e "Products already loaded ... $Y SKIPPING $N"
+# fi
+
+# app_restart
+# print_total_time
+
+
+
 #!/bin/bash
 
 app_name=catalogue
@@ -14,10 +44,10 @@ VALIDATE $? "Added Mongo repo"
 dnf install mongodb-mongosh -y &>>$LOGS_FILE
 VALIDATE $? "Installed MongoDB client"
 
-INDEX=$(mongosh --host mongodb.daws90s.shop --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+INDEX=$(mongosh --host mongodb.daws8486.online --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 
 if [ $INDEX -lt 0 ]; then
-    mongosh --host mongodb.daws90s.shop </app/db/master-data.js &>>$LOGS_FILE
+    mongosh --host mongodb.daws8486.online </app/db/master-data.js &>>$LOGS_FILE
     VALIDATE $? "Load Products"
 else
     echo -e "Products already loaded ... $Y SKIPPING $N"
@@ -25,3 +55,5 @@ fi
 
 app_restart
 print_total_time
+
+
